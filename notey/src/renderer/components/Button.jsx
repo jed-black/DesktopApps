5d68a23c5d8c68713a20f8795fa125a9fdc37179
Jed B <jedb@Jeds-MacBook-Air.local>
2024-08-11 23:24:17 -0600

    refactored from docker to local

--- notey/src/renderer/components/Button.jsx
+++ notey/src/renderer/components/Button.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react';
 
-const Button = ({ name }) => {
+const Button = ({ name, onClick }) => {
   return (
-    <button>{name}</button>
-  )
-}
+    <div className="submit">
+      <button onClick={onClick}>{name}</button>
+    </div>
+  );
+};
 
-export default Button
+export default Button;
--- notey/src/renderer/components/Main.jsx
+++ notey/src/renderer/components/Main.jsx
@@ -5,6 +5,11 @@ import Notebox from './Notebox';
 import Sidebar from './Sidebar';
 
 const Main = () => {
+
+  const onClick = () => {
+    alert("Submitting data");
+  }
+
   return (
     <div className="page">
       <div className="sidebar">
@@ -13,7 +18,7 @@ const Main = () => {
       <div className="main">
         <Title title={"Notey"}/>
         <Notebox/>
-        <Button name={"Submit"}/>
+        <Button name={"Submit"} onClick={onClick}/>
       </div>
     </div>
   )
